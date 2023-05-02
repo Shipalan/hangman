53234ca38862ff04bcb2a2baaf0fc858f4eeb020
Shipalan <106115938+Shipalan@users.noreply.github.com>
2023-05-02 15:07:57 -0600

    breaking out the functions and cleaning up the code.

---
+++ src/functions/Hangman.tsx
@@ -0,0 +1,28 @@
+const updateHangmanVisual = () => {
+  incorrectGuesses.forEach((letter, index) => {
+    console.log(index, letter);
+    switch (index) {
+      case 0:
+        setHangman([<div id="head"></div>]);
+
+        break;
+      case 1:
+        setHangman([...hangman, <div id="torso"></div>]);
+        break;
+      case 2:
+        setHangman([...hangman, <div id="leftArm"></div>]);
+        break;
+      case 3:
+        setHangman([...hangman, <div id="rightArm"></div>]);
+        break;
+      case 4:
+        setHangman([...hangman, <div id="leftLeg"></div>]);
+        break;
+      case 5:
+        setHangman([...hangman, <div id="rightLeg"></div>]);
+        break;
+      default:
+        break;
+    }
+  });
+};
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,14 +1,20 @@
-import { render } from "@testing-library/react";
-import "./home.css";
+import "../../App.css";
 import { useFormik } from "formik";
 import { useState, useEffect, ReactElement } from "react";
+import GuessForm from "../../functions/GuessForm";
+import UpdateGuesses from "../../functions/UpdateGuesses";
+import renderFormInput from "../../functions/RenderFormInput";
 const randomWords = require("random-words");
 
-const Home = () => {
-  //#1 Generate a word to guess
-  // console.log(randomWords())
-  // let word = String(randomWords());
+export type wordToGuess = string[];
+export type correctGuess = string[];
+export type incorrectGuesses = string[];
+export type setIncorrectGuesses = React.Dispatch<
+  React.SetStateAction<string[]>
+>;
+export type setCorrectGuesses = React.Dispatch<React.SetStateAction<string[]>>;
 
+const Home = () => {
   let [wordToGuess, setWordToGuess] = useState<string[]>([""]);
 
   let [correctGuess, setCorrectGuess] = useState<string[]>([]);
@@ -31,40 +37,34 @@ const Home = () => {
 
   //#2 update variable wordToGuess, state.
 
-  const updateHangmanVisual = () => {
-    incorrectGuesses.forEach((letter, index) => {
-      console.log(index, letter);
-      switch (index) {
-        case 0:
-          setHangman([<div id="head"></div>]);
-          console.log(hangman);
-          break;
-        case 1:
-          setHangman([...hangman, <div id="torso"></div>]);
-          console.log(hangman);
-          break;
-        case 2:
-          setHangman([...hangman, <div id="leftArm"></div>]);
-          console.log(hangman);
-          break;
-        case 3:
-          setHangman([...hangman, <div id="rightArm"></div>]);
-          console.log(hangman);
-          break;
-        case 4:
-          setHangman([...hangman, <div id="leftLeg"></div>]);
-          console.log(hangman);
-          break;
-        case 5:
-          setHangman([...hangman, <div id="rightLeg"></div>]);
-          console.log(hangman);
-          break;
-        default:
-          console.log(index, "Default");
-          break;
-      }
-    });
-  };
+  // const updateHangmanVisual = () => {
+  //   incorrectGuesses.forEach((letter, index) => {
+  //     console.log(index, letter);
+  //     switch (index) {
+  //       case 0:
+  //         setHangman([<div id="head"></div>]);
+
+  //         break;
+  //       case 1:
+  //         setHangman([...hangman, <div id="torso"></div>]);
+  //         break;
+  //       case 2:
+  //         setHangman([...hangman, <div id="leftArm"></div>]);
+  //         break;
+  //       case 3:
+  //         setHangman([...hangman, <div id="rightArm"></div>]);
+  //         break;
+  //       case 4:
+  //         setHangman([...hangman, <div id="leftLeg"></div>]);
+  //         break;
+  //       case 5:
+  //         setHangman([...hangman, <div id="rightLeg"></div>]);
+  //         break;
+  //       default:
+  //         break;
+  //     }
+  //   });
+  // };
   useEffect(() => {
     updateHangmanVisual();
   }, [incorrectGuesses]);
@@ -97,58 +97,6 @@ const Home = () => {
     });
   };
 
-  const formik = useFormik({
-    initialValues: {
-      guess: "",
-    },
-    onSubmit: (values) => {
-      UpdateGuesses(values);
-    },
-  });
-
-  const UpdateGuesses = async (guesses: { guess: string }) => {
-    const guess = guesses.guess.toUpperCase();
-
-    if (wordToGuess.includes(guess)) {
-      setCorrectGuess([...correctGuess, guess]);
-    }
-
-    if (incorrectGuesses.length === 6) {
-      alert("Game Over! Generate new word!");
-    }
-    if (!wordToGuess.includes(guess)) {
-      setIncorrectGuess([...incorrectGuesses, guess]);
-    }
-  };
-
-  const renderFormInput = () => {
-    if (incorrectGuesses.length < 6) {
-      return (
-        <form onSubmit={formik.handleSubmit} id="guessForm">
-          <label htmlFor="guess">Enter your guess</label>
-          <input
-            type="text"
-            id="guess"
-            name="guess"
-            onChange={formik.handleChange}
-            value={formik.values.guess}
-            onBlur={formik.handleBlur}
-            maxLength={1}
-          />
-          <button type="submit" id="submitBtn">
-            Submit
-          </button>
-        </form>
-      );
-    } else {
-      return (
-        <div id="formPlaceHolder">
-          <h2 id="gameOver">Game Over! You suck Bitch!</h2>
-        </div>
-      );
-    }
-  };
-
   useEffect(() => {
     if (correctGuess === wordToGuess) {
       alert("You have won you little bitch!");
@@ -168,7 +116,13 @@ const Home = () => {
             {hangman}
             <div id="postBase"></div>
           </div>
-          {renderFormInput()}
+          {renderFormInput(
+            incorrectGuesses,
+            wordToGuess,
+            correctGuess,
+            setCorrectGuess,
+            setIncorrectGuess
+          )}
         </div>
         <div id="wordContainer">
           <div id="word">{guessWord()}</div>
